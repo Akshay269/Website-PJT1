d01b80a379e7e57306c328e848fc0b29b7f08e13
Akshay <aksrahangdale@gmail.com>
2023-03-09 02:34:37 +0530

    before deploy

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors=require("cors");
-const stripe = require('stripe')('sk_test_51McoGoSBTsTb4qbmPPogsJghMKYqj9cgUO24LvRHqioPBeKBEjFVLKomFXSzm77ZbplU58DUoeaZMJmkYG86DnDY00yGVYh28f');
+const stripe = require("stripe");
+const cors = require("cors");
+const path = require("path");
+("sk_test_51McoGoSBTsTb4qbmPPogsJghMKYqj9cgUO24LvRHqioPBeKBEjFVLKomFXSzm77ZbplU58DUoeaZMJmkYG86DnDY00yGVYh28f");
+const port = process.env.PORT || 5000;
 
 const dotenv = require("dotenv");
 
 dotenv.config();
-const userRoutes = require("./routes/userRoutes")
-const adminRoutes = require("./routes/adminRoutes")
-mongoose.set('strictQuery', true);
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+mongoose.set("strictQuery", true);
 
 try {
-  mongoose.connect(process.env.MONGO_URI);  
+  mongoose.connect(process.env.MONGO_URI);
   console.log("DB Connected");
 } catch (err) {
   console.log(err);
@@ -21,17 +24,17 @@ try {
 app.use(cors());
 app.use(express.json());
 
-const YOUR_DOMAIN = 'http://localhost:5000'
+const YOUR_DOMAIN = "http://localhost:5000";
 
-app.post('/create-checkout-session', async (req, res) => {
+app.post("/create-checkout-session", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
-        price: 'price_1Mcx77SBTsTb4qbmfjFfZuNh',
+        price: "price_1Mcx77SBTsTb4qbmfjFfZuNh",
         quantity: 1,
       },
     ],
-    mode: 'payment',
+    mode: "payment",
     success_url: `http://localhost:5000/`,
     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
@@ -41,7 +44,14 @@ app.post('/create-checkout-session', async (req, res) => {
 
 app.use("/api/user", userRoutes);
 // app.use("/api/admin", adminRoutes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
-app.listen(5000, () => {
+app.post("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+app.listen(port, () => {
   console.log("Backend Server running ");
 });
